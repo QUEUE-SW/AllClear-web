62d3da36310add76e14e193b2b0c3c93dcc145f9
jihyun132 <jihyun.q.s@gmail.com>
2025-05-11 00:29:29 +0900

    feat: 버튼 핸들링

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const inputStyle =
     "w-full px-4 py-2 lg:py-3 bg-gray-200 2xl:text-lg lg:text-sm text-gray-500 border border-gray-300 rounded-3xl";
   const lineStyle = "grow border-t border-gray-300";
+
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    stdId: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if(!['stdId', 'password'].includes(name)) return;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = () => {
+    console.log(formData);
+    alert("로그인 버튼이 클릭되었습니다.");
+  };
+  const toSignup = () => navigate("/signup");
 
   return (
     // 중앙정렬
@@ -15,14 +37,27 @@ const LoginPage = () => {
         </div>
         {/* 폼 */}
         <div className="space-y-2 2xl:space-y-3">
-          <input type="text" className={inputStyle} placeholder="학번" />
+          <input
+            type="text"
+            name="stdId"
+            value={formData.stdId}
+            onChange={handleChange}
+            className={inputStyle}
+            placeholder="학번"
+          />
           <input
             type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
             className={inputStyle}
             placeholder="비밀번호"
           />
         </div>
-        <div className="cursor-pointer p-2 2xl:p-4 my-5 2xl:my-8 bg-indigo-500 text-white text-center rounded-lg shadow-lg shadow-indigo-500/50 hover:shadow-indigo-600/70 hover:shadow-[inset_10px_10px_10px]">
+        <div
+          className="cursor-pointer p-2 2xl:p-4 my-5 2xl:my-8 bg-indigo-500 text-white text-center rounded-lg shadow-lg shadow-indigo-500/50 hover:shadow-indigo-600/70 hover:shadow-[inset_10px_10px_10px]"
+          onClick={handleSubmit}
+        >
           로그인
         </div>
         <div className="flex items-center mx-5 text-gray-400 lg:text-sm 2xl:text-md">
@@ -30,7 +65,10 @@ const LoginPage = () => {
           <span className="mx-5 2xl:my-3">또는</span>
           <hr className={lineStyle} />
         </div>
-        <div className="cursor-pointer p-2 2xl:p-4 my-5 2xl:my-8 bg-white text-center rounded-lg shadow-gray-300/50 hover:shadow-indigo-300/50 shadow-[inset_3px_3px_3px_rgba(0,0,0,5)] hover:shadow-[10px_10px_10px,inset_1px_1px_5px]">
+        <div
+          className="cursor-pointer p-2 2xl:p-4 my-5 2xl:my-8 bg-white text-center rounded-lg shadow-gray-300/50 hover:shadow-indigo-300/50 shadow-[inset_3px_3px_3px_rgba(0,0,0,5)] hover:shadow-[10px_10px_10px,inset_1px_1px_5px]"
+          onClick={toSignup}
+        >
           회원가입
         </div>
       </div>
