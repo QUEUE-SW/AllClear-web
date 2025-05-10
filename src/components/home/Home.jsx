5256ef0cdfdbdc0df805cdb9e7f9b65d4be6584c
jihyun132 <jihyun.q.s@gmail.com>
2025-05-10 23:02:18 +0900

    feat: login페이지를 home페이지에 연결

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,18 @@
 // ./src/components/home/Home.jsx
 
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-	return <div className="bg-yellow-200 text-3xl text-red-500 p-4">Home</div>;
+	const navigate = useNavigate();
+	const toLogin = () => {
+		navigate("/login");
+	}
+  return (
+    <div>
+      <div className="bg-yellow-200 text-3xl text-red-500 p-4">Home</div>
+			<button className="p-4 border hover:bg-slate-500" onClick={toLogin}>Login</button>
+    </div>
+  );
 };
 
 export default Home;
--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import home from "@/router/home";
+import LoginPage from "@/pages/LoginPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     children: [...home],
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>,
   },
 ]);
 
