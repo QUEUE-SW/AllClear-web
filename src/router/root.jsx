414516787266dc5148cc034396dc51dd401d0a8f
Jiwon Park <parkjiwon0821@naver.com>
2025-05-11 10:59:39 +0900

    [QUEUE-104] 회원가입 페이지 UI 구현 (#3)
    * feat: SignUpForm 컴포넌트 레이아웃 고정 및 UI 최적화
    
    SignUpForm 컴포넌트의 높이와 너비를 450px * 590px로 고정하고,
    내부 input 요소들의 padding을 조정하여 스크롤 없이 모든 요소가 표현되도록 개선.
    버튼 화면 해상도에 맞게 간결화.
    
    Resolves: #QUEUE-104
    
    * refactor: handlePasswordConfirmChange 이름 수정
    
    변수 가독성을 위해 handlePWConfirmChange로 줄였습니다.
    
    Resolves: #QUEUE-104
    
    * design: 그림자 효과 변경
    
    회원가입 폼 컨테이너와 버튼의 그림자 효과를 변경하였습니다.
    
    Resolves: #QUEUE-104

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import home from "@/router/home";
+import auth from "./auth";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    children: [...home],
-  },
+	{
+		path: "/",
+		children: [...home, ...auth],
+	},
 ]);
 
 export default router;
