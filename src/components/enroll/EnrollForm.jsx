af690e22344118405f0c6644c9ac9e55a37232a3
jihyun132 <jihyun.q.s@gmail.com>
2025-05-20 21:22:22 +0900

    feat: 수강신청 현황 조회 api 구현

--- src/components/enroll/EnrollForm.jsx
+++ src/components/enroll/EnrollForm.jsx
@@ -1,96 +1,50 @@
 import React from "react";
 import CourseCard from "./CourseCard";
 import Filter from "./Filter";
+import { getEnrollStatus } from "@/services/courses";
+import { useEffect } from "react";
+import { useState } from "react";
+import { mockCourse, mockRegister } from "@/data/mockCourses";
 
 const EnrollForm = () => {
-  const mockCourse = [
-    {
-      courseId: 1,
-      courseCode: "cs101",
-      name: "운영체제",
-      professor: "박대영",
-      location: "IT관 117",
-      capacity: "20/20",
-      credit: 3,
-      time1: "월 10:00~11:30",
-      time2: "수 12:00~13:30",
-      status: "red",
-    },
-    {
-      courseId: 2,
-      courseCode: "cs102",
-      name: "컴퓨터프로그래밍",
-      professor: "박대영",
-      location: "IT관 117",
-      capacity: "20",
-      credit: 3,
-      time1: "월 10:00~11:30",
-      time2: "수 12:00~13:30",
-      status: "green",
-    },
-    {
-      courseId: 3,
-      courseCode: "cs103",
-      name: "컴퓨터프로그래밍",
-      professor: "박대영",
-      location: "IT관 117",
-      capacity: "20",
-      credit: 3,
-      time1: "월 10:00~11:30",
-      time2: "수 12:00~13:30",
-      status: "orange",
-    },
-    {
-      courseId: 4,
-      courseCode: "cs103",
-      name: "컴퓨터프로그래밍",
-      professor: "박대영",
-      location: "IT관 117",
-      capacity: "20",
-      credit: 3,
-      time1: "월 10:00~11:30",
-      time2: "수 12:00~13:30",
-      status: "gray",
-    },
-  ];
-  const mockRegister = [
-    {
-      courseId: 1,
-      courseCode: "cs101",
-      name: "운영체제",
-      professor: "박대영",
-      location: "IT관 117",
-      capacity: "20/20",
-      credit: 3,
-      time1: "월 10:00~11:30",
-      time2: "수 12:00~13:30",
-    },
-    {
-      courseId: 2,
-      courseCode: "cs102",
-      name: "컴퓨터프로그래밍",
-      professor: "박대영",
-      location: "IT관 117",
-      capacity: "20",
-      credit: 3,
-      time1: "월 10:00~11:30",
-      time2: "수 12:00~13:30",
-    },
-  ];
+  const [generalCourses, setGeneralCourses] = useState(mockCourse);
+  const [registerCourses, setRegisterCourses] = useState(mockRegister);
+
+  const getRegisterCourses = async () => {
+    try {
+      const res = await getEnrollStatus();
+      setRegisterCourses(res);
+    } catch (error) {
+      console.error("수강신청 현황 조회 실패", error);
+    }
+  };
+
+  useEffect(() => {
+    getRegisterCourses();
+  }, []);
+
   return (
     <div className="w-[1050px]">
       {/* filters */}
       <div className="flex gap-12 mb-5">
-        <Filter filter="category"/>
-        <Filter filter="grade"/>
-        <Filter filter="department"/>
-        <Filter filter="code"/>
+        <Filter filter="category" />
+        <Filter filter="grade" />
+        <Filter filter="department" />
+        <Filter filter="code" />
       </div>
       <div className="flex gap-6 min-w-0">
         {/* cources */}
-        <CourseCard title="강의 목록" courses={mockCourse} isRegister={false}/>
+        <CourseCard
+          title="강의 목록"
+          courses={generalCourses}
+          isRegister={false}
+        />
         {/* register */}
-        <CourseCard title="수강 현황" courses={mockRegister} isRegister={true}/>
+        <CourseCard
+          title="수강 현황"
+          courses={registerCourses}
+          isRegister={true}
+        />
       </div>
     </div>
   );
