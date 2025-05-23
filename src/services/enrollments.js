8311a7a2093a57b22f7c62f77f63b8c16121f4ea
Jiwon Park <parkjiwon0821@naver.com>
2025-05-23 13:55:03 +0900

    docs: 서비스함수 주석 추가

--- src/services/enrollments.js
+++ src/services/enrollments.js
@@ -12,6 +12,10 @@ export const enrollCourse = async (courseId) => {
   return response.data;
 }
 
+/**
+ * 강의 신청 취소 요청
+ * @param {number} courseId 신청할 강의 ID
+ */
 export const cancelEnrollment = async (enrollmentId) => {
   const response = await apiInterface("delete", `/enrollments/${enrollmentId}`);
 
