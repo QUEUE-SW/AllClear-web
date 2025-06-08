202b7c0f8c781d6cf30a99815b9eb1d7f692eef2
Jiwon Park <parkjiwon0821@naver.com>
2025-06-08 21:30:14 +0900

    design: 전체  반응형 추가

--- src/pages/EnrollPage.jsx
+++ src/pages/EnrollPage.jsx
@@ -27,7 +27,7 @@ const EnrollPage = () => {
   }, []);
 
   return (
-    <div className="w-full h-screen flex flex-col bg-gradient-to-b from-blue-50 to-blue-200">
+    <div className="w-full min-h-screen flex flex-col bg-gradient-to-b from-blue-50 to-blue-200">
       {/* 고정 헤더 */}
       <Header myBasic={myBasic} />
 
