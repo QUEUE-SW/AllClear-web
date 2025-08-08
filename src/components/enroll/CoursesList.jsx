7b599b55fb9f89566dbfdf23ff4f1bf8357a8a0c
mac <mac@macui-MacBook-Pro.local>
2025-08-08 21:47:49 +0900

    feature: 수강목록 조회 sse 연결

--- src/components/enroll/CoursesList.jsx
+++ src/components/enroll/CoursesList.jsx
@@ -1,6 +1,40 @@
 import CourseItem from "@/components/enroll/CourseItem";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const CoursesList = ({ courses, capacities, onEnrollSuccess }) => {
+  const [capa, setCapa] = useState(capacities || []);
+
+  useEffect(() => {
+    if (!courses) return;
+
+    const courseIds = courses.map((c) => `courseIds=${c.courseId}`).join("&");
+
+    const eventSource = new EventSource(
+      `${import.meta.env.VITE_API_BASE_SSE_URL}/api/v1/seats?${courseIds}`
+    );
+
+    if (!eventSource) return;
+
+    eventSource.onopen = () => {
+      console.log("SSE 연결");
+    };
+
+    eventSource.addEventListener("seat", (event) => {
+      const data = JSON.parse(event.data);
+      console.log("seat 데이터: ", data);
+      setCapa((prevCapa) =>
+        prevCapa.map((capa) =>
+          capa.courseId === data.courseId
+            ? { ...capa, participant: data.remaining }
+            : capa
+        )
+      );
+    });
+
+    return () => eventSource.close();
+  }, [courses]);
+
   return (
     <div
       className="w-[814px] h-[607px] overflow-y-auto
@@ -11,9 +45,7 @@ const CoursesList = ({ courses, capacities, onEnrollSuccess }) => {
     >
       <div className="grid grid-cols-3 gap-x-2 gap-y-3">
         {courses?.map((course) => {
-          const current = capacities.find(
-            (c) => c.courseId === course.courseId
-          );
+          const current = capa.find((c) => c.courseId === course.courseId);
           return (
             <CourseItem
               key={course.courseId}
